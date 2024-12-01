eda5140e446f0db2e92ee12b81ea5b71314784f2
psedukhina <sedukhina.polina@gmail.com>
2024-12-01 21:16:29 +0100

    [Day 1] Implemented solution for the second part

--- day1/main.py
+++ day1/main.py
@@ -2,15 +2,23 @@ import sys
 
 
 def calculate_distance(list_1: list[int], list_2: list[int]) -> int:
-    if len(list_1) != len(list_2):
-        raise ValueError("Lists must have the same length")
-
+    # Distance is a sum of absolute differences between elements of two sorted lists
     list_1.sort()
     list_2.sort()
     return sum(abs(a - b) for a, b in zip(list_1, list_2))
 
 
-def solve(filename: str) -> int:
+def calculate_similarity_score(list_1: list[int], list_2: list[int]) -> int:
+    # Similarity score is a sum a number of the first list
+    # multiplied by number of its occurrence in the second list
+    score = 0
+    for number in list_1:
+        score += list_2.count(number) * number
+
+    return score
+
+
+def solve(filename: str) -> (int, int):
     list_1, list_2 = [], []
     with open(filename, 'r') as file:
         for line in file:
@@ -22,14 +30,18 @@ def solve(filename: str) -> int:
             list_1.append(a)
             list_2.append(b)
 
-    return calculate_distance(list_1, list_2)
+    if len(list_1) != len(list_2):
+        raise ValueError("Lists must have the same length")
+
+    return calculate_distance(list_1, list_2), calculate_similarity_score(list_1, list_2)
 
 
 def main() -> None:
     filename = "data.txt" if len(sys.argv) == 1 else sys.argv[1]
     try:
-        result = solve(filename=filename)
-        print(f"Result: {result}")
+        distance, similarity_score = solve(filename=filename)
+        print(f"Distance: {distance}")
+        print(f"Similarity score: {similarity_score}")
     except Exception as e:
         print(f"An error occurred: {e}")
         sys.exit(1)
--- day1/test.py
+++ day1/test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from day1.main import calculate_distance, solve
+from day1.main import calculate_distance, calculate_similarity_score, solve
 
 
 @pytest.mark.parametrize(
@@ -21,23 +21,28 @@ def test_calculate_distance(list_1, list_2, expected_result):
 
 
 @pytest.mark.parametrize(
-    "list_1, list_2",
+    "list_1, list_2, expected_result",
     [
-        ([1, 2], [1]),
-        ([], [1, 2]),
+        ([2], [3], 0),
+        ([2, 1], [3, 4], 0),
+        ([2, 1, 3], [3, 4, 5], 3),
+        ([], [], 0),
+        ([10, -5], [5, 10], 10),
+        ([0, 0, 0], [0, 0, 0], 0),
+        ([5, 8, 3], [2, 5, 10], 5),
+        ([3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3], 31),
     ]
 )
-def test_calculate_distance_invalid(list_1, list_2):
-    with pytest.raises(ValueError):
-        calculate_distance(list_1, list_2)
+def test_calculate_similarity_score(list_1, list_2, expected_result):
+    assert calculate_similarity_score(list_1, list_2) == expected_result
 
 
 @pytest.mark.parametrize(
     "file_input, expected_result",
     [
-        ("3 4\n4 3\n2 5\n1 3\n3 9\n3 3", 11),
-        ("1 1\n2 2\n3 3", 0),
-        ("10 20\n30 40", 20),
+        ("3 4\n4 3\n2 5\n1 3\n3 9\n3 3", (11, 31)),
+        ("1 1\n2 2\n3 3", (0, 6)),
+        ("10 20\n30 40", (20, 0)),
     ]
 )
 def test_solve_valid(file_input, expected_result, tmp_path):
@@ -52,7 +57,7 @@ def test_solve_valid(file_input, expected_result, tmp_path):
         ("3 4\n4 3\n2 5\n1 3\n3 9\n3", 11),
     ]
 )
-def test_solve_valid(file_input, expected_result, tmp_path):
+def test_solve_invalid(file_input, expected_result, tmp_path):
     input_file = tmp_path / "data.txt"
     input_file.write_text(file_input)
     with pytest.raises(ValueError):
