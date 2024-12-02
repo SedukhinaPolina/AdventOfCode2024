bada256bcaca0dd0cd4fee2aca1ba12ab4b58346
psedukhina <sedukhina.polina@gmail.com>
2024-12-02 23:25:02 +0100

    [Day 2] Implemented solution for the second part

--- day2/main.py
+++ day2/main.py
@@ -19,12 +19,24 @@ def _is_report_safe(report: list[int]) -> bool:
     return True
 
 
-def solve(filename: str) -> (int, int):
+def check_report(report: list[int], with_dampener: bool) -> bool:
+    is_report_safe = _is_report_safe(report=report)
+    if is_report_safe:
+        return True
+
+    if with_dampener:
+        for i in range(len(report)):
+            is_report_safe = _is_report_safe(report[:i] + report[i + 1:])
+            if is_report_safe:
+                return True
+
+
+def solve(filename: str, with_dampener: bool) -> (int, int):
     num_of_safe_reports = 0
     with open(filename, 'r') as file:
         for line in file:
             numbers = line.split()
-            if _is_report_safe(report=list(map(int, numbers))):
+            if check_report(report=list(map(int, numbers)), with_dampener=with_dampener):
                 num_of_safe_reports += 1
 
     return num_of_safe_reports
@@ -33,7 +45,7 @@ def solve(filename: str) -> (int, int):
 def main() -> None:
     filename = "data.txt" if len(sys.argv) == 1 else sys.argv[1]
     try:
-        num_of_safe_reports = solve(filename=filename)
+        num_of_safe_reports = solve(filename=filename, with_dampener=True)
         print(f"# or safe reports: {num_of_safe_reports}")
     except Exception as e:
         print(f"An error occurred: {e}")
--- day2/test.py
+++ day2/test.py
@@ -11,7 +11,22 @@ from day2.main import solve
         ("7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9", 2),
     ]
 )
-def test_solve_valid(file_input, expected_result, tmp_path):
+def test_solve(file_input, expected_result, tmp_path):
     input_file = tmp_path / "data.txt"
     input_file.write_text(file_input)
-    assert solve(filename=str(input_file)) == expected_result
+    assert solve(filename=str(input_file), with_dampener=False) == expected_result
+
+
+@pytest.mark.parametrize(
+    "file_input, expected_result",
+    [
+        ("18 21 22 24 27 30 35", 1),
+        ("3 4 1\n4 3 3\n2 5 10\n1 3 5\n3 9 1\n3 3 4", 6),
+        ("1 2\n2 2\n3 4", 2),
+        ("7 6 4 2 1\n1 2 7 8 9\n9 7 6 2 1\n1 3 2 4 5\n8 6 4 4 1\n1 3 6 7 9", 4),
+    ]
+)
+def test_solve_with_dampener(file_input, expected_result, tmp_path):
+    input_file = tmp_path / "data.txt"
+    input_file.write_text(file_input)
+    assert solve(filename=str(input_file), with_dampener=True) == expected_result
